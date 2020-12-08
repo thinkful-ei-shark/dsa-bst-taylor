6e85e5554d85a63f24941f733099d64a64491121
Taylor Phelps <tphelps1993@gmail.com>
2020-12-08 11:48:44 -0500

    updated

---
+++ bst-drills.js
@@ -0,0 +1,26 @@
+const BinarySearch = require('./dsa-bst');
+
+function main() {
+  const BST = new BinarySearch();
+
+  BST.insert('E');
+  BST.insert('A');
+  BST.insert('S');
+  BST.insert('Y');
+  BST.insert('Q');
+  BST.insert('U');
+  BST.insert('E');
+  BST.insert('S');
+  BST.insert('T');
+  BST.insert('I');
+  BST.insert('O');
+  BST.insert('I');
+  BST.insert('N');
+
+
+
+
+  console.log(BST);
+}
+
+main();
